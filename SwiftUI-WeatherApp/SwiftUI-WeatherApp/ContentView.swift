2fe646e058f16b6d2c7ed0f93efb76be8d1c5cb4
Vijay Sharma <113002348+VijaySharma898@users.noreply.github.com>
2024-07-07 22:09:42 +0530

    weekday weather added

--- SwiftUI-WeatherApp/SwiftUI-WeatherApp/ContentView.swift
+++ SwiftUI-WeatherApp/SwiftUI-WeatherApp/ContentView.swift
@@ -15,12 +15,62 @@ struct ContentView: View {
                 
                 CityNameView(cityName: "Cupertino, CA")
                 WeatherView(temperature: "76°", icon:  "cloud.sun.fill")
+                    
+                HStack(spacing: 20) {
+                    WeekWeather(weekName: "Mon",
+                                imageName: "sun.max.fill",
+                                temperature: 76)
+                    
+                    WeekWeather(weekName: "Tue",
+                                imageName: "cloud.sun.fill",
+                                temperature: 60)
+                    
+                    WeekWeather(weekName: "Wed", 
+                                imageName: "wind",
+                                temperature: 72)
+                    
+                    WeekWeather(weekName: "Thu",
+                                imageName: "sunset.fill",
+                                temperature: 75)
+                    
+                    WeekWeather(weekName: "Fri", 
+                                imageName: "cloud.sun.fill",
+                                temperature: 72)
+
+                }
                 Spacer()
             }
             
         }
     }
 }
+struct WeekWeather: View {
+    
+    var weekName: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Text(weekName)
+                .font(.system(size: 20, weight: .bold))
+                .foregroundStyle(.white)
+                .padding(.bottom)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundStyle(.white)
+            
+        }
+        
+    }
+}
+
 struct CityNameView: View {
     
     var cityName: String
@@ -57,7 +107,9 @@ struct WeatherView: View {
                 .font(.system(size: 70, weight: .medium))
                 .foregroundStyle(.white)
         }
+        .padding(.bottom, 40)
     }
+        
 }
 
 #Preview {
